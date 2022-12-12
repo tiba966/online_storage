1bbec8b778a2784ad0b36a4d7b0f3043156e5ae2
g.ibrahim <g.ibrahim@singleclic.com>
2022-12-12 21:53:32 +0300

    edit profile

--- staticfile/top_header.php
+++ staticfile/top_header.php
@@ -73,10 +73,27 @@
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Processing under requests</span></a>
             </li>
+
+                  <!-- Divider -->
+ <hr class="sidebar-divider  my-0">
+   <!-- Nav Item - Charts -->
+   <li class="nav-item">
+                <a class="nav-link" href="profile.php">
+                    <i class="fas fa-fw fa-user-alt"></i>
+                    <span>Profile</span></a>
+            </li>
 <!-- Divider -->
 <hr class="sidebar-divider">
 
-         
+   <!-- Nav Item - Charts -->
+   <li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
+                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    <span>Logout</span></a>
+            </li>
+<!-- Divider -->
+<hr class="sidebar-divider">
+
 
             <!-- Divider -->
 
@@ -193,7 +210,7 @@
                                 <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
                             </div>
                         </li>
-
+                        <div class="topbar-divider d-none d-sm-block"></div>
                         <!-- Nav Item - Messages -->
                         <li class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
@@ -260,32 +277,9 @@
                             </div>
                         </li>
 
-                        <div class="topbar-divider d-none d-sm-block"></div>
+                       
 
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Ali Ahmed </span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/s11.jpeg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                              
-                               
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
+                        
 
                     </ul>
 
